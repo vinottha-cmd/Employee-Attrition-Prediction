c1e61d9abf0013757c7789655e78a60a74dfebb1
vinottha-cmd <vinottha@graduate.utm.my>
2026-07-17 15:37:19 +0800

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def test_no_missing_values():
-    df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
+    df = pd.read_csv("Employee-Attrition-Performance.csv")
     assert df.isnull().sum().sum() == 0
